5de45e86c220513c81cb74c51f83b2743e73625a
Thiago Pereira <thiago.dev.brz@gmail.com>
2024-07-17 22:04:40 -0300

    feat: first commit

---
+++ cmd/server/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/thiagodevbrz/go-graphql/graph"
+	"github.com/thiagodevbrz/go-graphql/internal/database"
+)
+
+const defaultPort = "8080"
+
+func main() {
+
+	db, err := sql.Open("sqlite3", "./data.db")
+
+	if err != nil {
+		log.Fatal("Failed to open the database: %w", err)
+	}
+
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDb: categoryDb,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
